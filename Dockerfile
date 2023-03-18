6afc14b3c4af404315b904fca6ea801950cd61a4
rakeshmahajan9657 <121rakeshmahajan@gmail.com>
2023-03-18 16:41:53 +0530

    new

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM openjdk:19
 EXPOSE 8085
 ADD target/demo.jar demo.jar
 ENTRYPOINT ["java", "-jar","/demo.jar"]
--- src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
+++ src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -1,18 +1,6 @@
 package com.example.easynotes;
 
-import com.example.easynotes.model.Note;
-import com.example.easynotes.repository.NoteRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-@SpringBootTest
 public class EasyNotesApplicationTests {
 
 
